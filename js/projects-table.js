f7cda5675f31105e089d67ff3399903dd65c759a
Boris Pek <tehnick-8@yandex.ru>
2016-11-09 04:25:44 +0300

    Small code refactoring in projects-table.js

--- js/projects-table.js
+++ js/projects-table.js
@@ -1,9 +1,9 @@
 // License: MIT (Expat)
 
 function update_cells_width() {
-    list = [".role", ".last_version", ".last_timestamp", ".builds_number"];
+    var list = [".role", ".last_version", ".last_timestamp", ".builds_number"];
     for (var k in list) {
-        width = 0;
+        var width = 0;
         $.each($("#projects_table_container").find(list[k]), function(i, item) {
             width = Math.max(width, $(item).width());
         });
@@ -15,10 +15,10 @@ function update_cells_width() {
 }
 
 function add_item_into_projects_table(i, item) {
-    num = +localStorage.projects_number;
+    var num = +localStorage.projects_number;
     localStorage.projects_number = num + 1;
 
-    last_status = "?";
+    var last_status = "?";
     if (item.last_status === "passed")
         last_status = "✔";
     else if (item.last_status === "failed")
@@ -26,24 +26,24 @@ function add_item_into_projects_table(i, item) {
     else if (item.last_status === "started")
         last_status = "⚫";
 
-    html =  "<div id=\"" + item.project_name + "\" class=\"table_item " +
-            item.last_status + "\">\n" +
-                "<li class=\"last_status\">\n" + last_status + "</li>\n" +
-                "<li class=\"project_name\">\n" +
-                    "<a class=\"" + item.last_status + "\" " + "href=\"" +
-                    $("#projects_page").find("a").attr("href") +
-                    "/" + item.project_name + "\">" + item.project_name + "</a>\n" +
-                "</li>\n" +
-                "<li class=\"builds_number\">\n" + item.builds_number + "</li>\n" +
-                "<li class=\"last_version\">\n" +
-                    "<a class=\"" + item.last_status + "\" " + "href=\"" +
-                    $("#projects_page").find("a").attr("href") +
-                    "/" + item.project_name + "/" + item.last_version + "\">" +
-                    item.last_version + "</a>\n" +
-                "</li>\n" +
-                "<li class=\"last_timestamp\">\n" + item.last_timestamp + "</li>\n" +
-                "<li class=\"role\">\n" + $("#" + item.role).text() + "</li>\n" +
-            "</div>\n";
+    var html =  "<div id=\"" + item.project_name + "\" class=\"table_item " +
+                item.last_status + "\">\n" +
+                    "<li class=\"last_status\">\n" + last_status + "</li>\n" +
+                    "<li class=\"project_name\">\n" +
+                        "<a class=\"" + item.last_status + "\" " + "href=\"" +
+                        $("#projects_page").find("a").attr("href") +
+                        "/" + item.project_name + "\">" + item.project_name + "</a>\n" +
+                    "</li>\n" +
+                    "<li class=\"builds_number\">\n" + item.builds_number + "</li>\n" +
+                    "<li class=\"last_version\">\n" +
+                        "<a class=\"" + item.last_status + "\" " + "href=\"" +
+                        $("#projects_page").find("a").attr("href") +
+                        "/" + item.project_name + "/" + item.last_version + "\">" +
+                        item.last_version + "</a>\n" +
+                    "</li>\n" +
+                    "<li class=\"last_timestamp\">\n" + item.last_timestamp + "</li>\n" +
+                    "<li class=\"role\">\n" + $("#" + item.role).text() + "</li>\n" +
+                "</div>\n";
     $("#projects_table").append(html);
     update_cells_width($("#" + item.project_name));
 
@@ -55,15 +55,15 @@ function add_item_into_projects_table(i, item) {
 }
 
 function fill_in_projects_table(data) {
-    array = $.parseJSON(data);
+    var array = $.parseJSON(data);
     $.each(array, add_item_into_projects_table);
     update_cells_width();
 }
 
 function show_more_projects() {
-    len = +localStorage.projects_per_page;
-    pos = +localStorage.projects_number;
-    data = "pos=" + pos + "&len=" + len;
+    var len = +localStorage.projects_per_page;
+    var pos = +localStorage.projects_number;
+    var data = "pos=" + pos + "&len=" + len;
     $.post("./?ajax=projects_list", data, fill_in_projects_table);
     return false;
 }
